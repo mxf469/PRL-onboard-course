91abf55cfa5b4dbd3379ca0d0c888de31e603eb3
mxf469 <30603498+mxf469@users.noreply.github.com>
2017-08-01 10:29:11 -0400

    this is a test

---
+++ this.py
@@ -0,0 +1,31 @@
+
+# this is a comment
+
+this_part = "is a string" #comment
+print (this_part[1])
+print (this_part[-2])
+print(len(this_part))
+print(this_part[2:8:2]) #[start:stop:step]
+print(this_part[::-1])  #reverse order
+#print(this_part[].isdigit)
+
+some_data= "x={0} y={1}"
+print(some_data.format(2, 5))
+
+
+h_bar = 1.05457e-34
+pi = 3.14159
+print ("h_bar is :")
+print (h_bar)
+x=2*pi*h_bar
+print("h is:")
+print(x)
+print ("h_bar is a:")
+print(type (h_bar))
+
+my_long_string = """stuff
+more stuff
+even more
+"""
+print("hello")
+
